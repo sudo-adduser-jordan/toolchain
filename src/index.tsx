c7f0fd0cadcb9c2144fd7d1489c524500c26d649
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-11 14:52:28 -0700

    Feature: Add Type Check
    -configure type check script

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,4 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
