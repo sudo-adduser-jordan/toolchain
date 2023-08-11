7b00d546cac45ad09aadd97b3faba57d304676a6
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-11 15:23:06 -0700

    Refactor: Landing Page
    -refactor landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import "./styles/app.css";
-import logo from "./assets/react.png";
+import "./app.css";
+import react from "./assets/react.png";
+import typescript from "./assets/typescript.png";
+import esbuild from "./assets/esbuild.png";
+import eslint from "./assets/eslint.png";
+import prettier from "./assets/prettier.png";
 
 export default function App() {
   return (
     <>
-      <div className="container">
+      <main className="container">
         <div className="title">Toolchain</div>
-        <div className="add">+</div>
-        <img className="image" src={logo} alt="" />
-      </div>
+        <section className="image-container">
+          <img src={react} alt="" style={{ width: "5rem", height: "5rem" }} />
+          <img src={typescript} alt="" style={{ width: "5rem", height: "5rem" }} />
+          <img src={esbuild} alt="" style={{ width: "5rem", height: "5rem" }} />
+          <img src={eslint} alt="" style={{ width: "5rem", height: "5rem" }} />
+          <img src={prettier} alt="" style={{ width: "5rem", height: "5rem" }} />
+        </section>
+      </main>
     </>
   );
 }
