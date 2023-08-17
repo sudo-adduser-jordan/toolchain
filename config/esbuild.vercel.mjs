f1e4b8d4da823ba81b9f72d3af552e2a086b23ab
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-16 17:05:17 -0700

    Fix: Vercel Build Error
    -update build script

--- config/esbuild.vercel.mjs
+++ config/esbuild.vercel.mjs
@@ -15,23 +15,10 @@ let result = await esbuild.build({
   bundle: true,
   sourcemap: false,
   minify: true,
-  // target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
   metafile: true,
   outdir: "./public",
   logLevel: "info",
 });
 
-// write meta file - this can be used at https://esbuild.github.io/analyze/
 fs.writeFileSync("./public/meta.json", JSON.stringify(result.metafile));
-
-/* print meta file results */
-
-// console.log(result);
-
 console.log(await esbuild.analyzeMetafile(result.metafile));
-
-// console.log(
-//   await esbuild.analyzeMetafile(result.metafile, {
-//     verbose: true,
-//   })
-// );
